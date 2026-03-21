0b092586332ee5fda271e8ceb276c5604929b850
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-03-21 13:42:17 -0300

    Create Pessoa.java

---
+++ Laboratorio-aulas/Abstracao-Pessoas/Pessoa/Pessoa.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Pessoa{
+
+    String nome;
+    int idade;
+    char sexo;
+    String telefone;
+
+
+    public Pessoa(String nome, int idade, char sexo, String telefone){
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.telefone = telefone;
+
+    }
+    @Override
+    public String toString() {
+    return "\n--- Dados da Pessoa ---" +
+           "\nNome: " + nome +
+           "\nIdade: " + idade +
+           "\nSexo: " + sexo +
+           "\nTelefone: " + telefone;
+}
+
+}
